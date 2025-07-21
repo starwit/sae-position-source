8b37056b3b5b5886ca5bfd934e05d0ca199f8fd3
Florian Stanek <flonix8@users.noreply.github.com>
2025-07-21 16:42:44 +0200

    Initial commit

---
+++ mystage/config.py
@@ -0,0 +1,25 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing_extensions import Annotated
+from visionlib.pipeline.settings import LogLevel, YamlConfigSettingsSource
+
+
+class RedisConfig(BaseModel):
+    host: str = 'localhost'
+    port: Annotated[int, Field(ge=1, le=65536)] = 6379
+    stream_id: str = 'stream1'
+    input_stream_prefix: str = 'objecttracker'
+    output_stream_prefix: str = 'mystage'
+
+class MyStageConfig(BaseSettings):
+    log_level: LogLevel = LogLevel.WARNING
+    redis: RedisConfig = RedisConfig()
+    prometheus_port: Annotated[int, Field(ge=1024, le=65536)] = 8000
+
+    model_config = SettingsConfigDict(env_nested_delimiter='__')
+
+    @classmethod
+    def settings_customise_sources(cls, settings_cls, init_settings, env_settings, dotenv_settings, file_secret_settings):
+        return (init_settings, env_settings, YamlConfigSettingsSource(settings_cls), file_secret_settings)
